db92fe3c3ef63142b342e0a281451e9903b87e72
FerozShaik96 <ferozshaik1997@gmail.com>
2024-07-07 11:36:13 +0530

    Fixed BackGround issue

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={`h-full bg-light-blue ${poppins.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
